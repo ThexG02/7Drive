7b371a46d7ce68560d72d0c3807fd038899e64d9
Harsh <harsh2020bhardwaj@gmail.com>
2025-02-02 17:28:55 +0530

    final edit

--- 7Drive/src/test/java/_Drive/example/_Drive/ApplicationTests.java
+++ 7Drive/src/test/java/_Drive/example/_Drive/ApplicationTests.java
@@ -14,7 +14,7 @@ class ApplicationTests {
 	@Test
 	void contextLoads() {
 		emailSenderService.sendEmail(
-				"harsh2020bhardwaj@gmail.com",
+				"abcd@gmail.com",
 				"testing email",
 				"testing email for 7 drive");
 	}
